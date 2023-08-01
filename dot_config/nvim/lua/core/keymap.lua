84b06bf8b6115612a82e35ac7056477d8ab0214e
peelz <peelz.dev@gmail.com>
2023-08-01 18:57:59 -0400

    vim: refactor core.keymap to use new lua-native APIs

--- dot_config/nvim/lua/core/keymap.lua
+++ dot_config/nvim/lua/core/keymap.lua
@@ -1,26 +1,4 @@
-local S = {
-  _fns = {},
-}
-
-local function create_mapping(mode, l, func, expr)
-  local func_name = mode .. "_" .. l
-  S._fns[func_name] = func
-
-  func_name = func_name
-    :gsub("'", "\\'")
-    :gsub('"', '\\"')
-    :gsub("<", "<lt>")
-    -- :gsub("%[", "[")
-    -- :gsub("%]", "]")
-
-  -- TODO: support expr mappings; couldn't get nvim_exec_lua() to work :(
-  local cmd = "<cmd>lua require('core.keymap')._fns['" .. func_name .. "']()<CR>"
-  if mode == "i" then
-    cmd = "<C-o>" .. cmd
-  end
-
-  return cmd
-end
+local S = {}
 
 local function get_opts(opts)
   opts = opts or {}
@@ -35,42 +13,13 @@ end
 
 function S.map_buf(bufnr, mode, l, r, opts)
   opts = get_opts(opts)
-
-  function map(mode)
-    local r = r
-    if type(r) == "function" then
-      r = create_mapping(mode, l, r)
-    end
-    vim.api.nvim_buf_set_keymap(bufnr, mode, l, r, opts)
-  end
-
-  if type(mode) == "table" then
-    for _, mode in ipairs(mode) do
-      map(mode)
-    end
-  else
-    map(mode)
-  end
+  opts.buffer = bufnr
+  vim.keymap.set(mode, l, r, opts)
 end
 
 function S.map(mode, l, r, opts)
   opts = get_opts(opts)
-
-  function map(mode)
-    local r = r
-    if type(r) == "function" then
-      r = create_mapping(mode, l, r)
-    end
-    vim.api.nvim_set_keymap(mode, l, r, opts)
-  end
-
-  if type(mode) == "table" then
-    for _, mode in ipairs(mode) do
-      map(mode)
-    end
-  else
-    map(mode)
-  end
+  vim.keymap.set(mode, l, r, opts)
 end
 
 return S
