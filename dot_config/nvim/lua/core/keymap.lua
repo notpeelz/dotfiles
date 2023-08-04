86455944234583991745ecb29bb556c6a68b3000
peelz <peelz.dev@gmail.com>
2023-08-04 19:40:14 -0400

    vim: fix broken command-mode mappings

--- dot_config/nvim/lua/core/keymap.lua
+++ dot_config/nvim/lua/core/keymap.lua
@@ -4,27 +4,36 @@ local function with_default_opts(mode, opts)
   if opts.noremap == nil and opts.remap == nil then
     opts.noremap = true
   end
+
   if opts.silent == nil then
-    opts.silent = true
+    local is_command_mapping = mode == "c"
+    if type(mode) == "table" and vim.tbl_contains(mode, "c") then
+      is_command_mapping = true
+    end
+
+    if not is_command_mapping then
+      opts.silent = true
+    end
   end
 end
 
 function S.mapping(o)
-  with_default_opts(o)
-  o.mode = table.remove(o, 1)
+  local mode = table.remove(o, 1)
+  with_default_opts(mode, o)
+  o.mode = mode
   return o
 end
 
 function S.map_buf(bufnr, mode, l, r, opts)
   local opts = opts or {}
-  with_default_opts(opts)
+  with_default_opts(mode, opts)
   opts.buffer = bufnr
   vim.keymap.set(mode, l, r, opts)
 end
 
 function S.map(mode, l, r, opts)
   local opts = opts or {}
-  with_default_opts(opts)
+  with_default_opts(mode, opts)
   vim.keymap.set(mode, l, r, opts)
 end
 
