6368aeb85bdd8e0e680a36124d6a6a0ba617a31c
peelz <peelz.dev@gmail.com>
2023-08-04 14:20:28 -0400

    vim: add suda plugin

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -5,6 +5,36 @@ return {
   "tpope/vim-surround",
   "tpope/vim-repeat",
   {
+    "lambdalisue/suda.vim",
+    config = function()
+      local cmds = {
+        { "W", "write" },
+        { "E", "edit" },
+        { "TabEdit", "tabedit" },
+      }
+      for _, cmd in ipairs(cmds) do
+        vim.api.nvim_create_user_command(cmd[1], function(p)
+          local args = string.len(p.args) == 0 and vim.fn.expand("%:p") or p.args
+          if string.len(args) == 0 then
+            vim.api.nvim_echo({
+              { "E32: No file name", "ErrorMsg" },
+            }, true, {})
+            return
+          end
+          if p.bang then
+            vim.api.nvim_command(cmd[2] .. "! suda://" .. args)
+          else
+            vim.api.nvim_command(cmd[2] .. " suda://" .. args)
+          end
+        end, {
+          complete = "file",
+          nargs = "?",
+          bang = true,
+        })
+      end
+    end,
+  },
+  {
     "inkarkat/vim-ReplaceWithRegister",
     keys = {
       keymap.mapping{ "n", "r", "<nop>" },
