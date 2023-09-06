d67ca0b37cc76de2236ec57d94dd540b6bac5d9e
peelz <peelz.dev@gmail.com>
2023-09-06 19:33:56 -0400

    vim: add nvim-unception plugin

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -26,6 +26,13 @@ end
 
 return {
   {
+    "samjwill/nvim-unception",
+    init = function()
+      vim.g.unception_open_buffer_in_new_tab = true
+      vim.g.unception_enable_flavor_text = false
+    end,
+  },
+  {
     "akinsho/toggleterm.nvim",
     init = function()
       au.group("FloatTermUnfocus", {
