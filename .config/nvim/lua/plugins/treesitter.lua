c6270618a7598512a50e2d618994d3b3fd12c00f
peelz <peelz.dev@gmail.com>
2024-09-26 01:37:33 -0400

    vim: use default lines/height treesitter-context settings

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -101,10 +101,7 @@ return plugins{
     },
     opts = {
       enable = true,
-      max_lines = 0,
-      min_window_height = 0,
       line_numbers = true,
-      multiline_threshold = 20,
       trim_scope = "outer",
       mode = "topline",
       separator = nil,
