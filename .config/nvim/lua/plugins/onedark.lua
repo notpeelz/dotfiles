d39b2a35fd61f4662ecd8e6aec50e102d4ef0ae3
peelz <peelz.dev@gmail.com>
2026-07-17 21:49:07 -0400

    vim: fix color scheme syncing

--- .config/nvim/lua/plugins/onedark.lua
+++ .config/nvim/lua/plugins/onedark.lua
@@ -1,3 +1,4 @@
+local au = require("core.au")
 local plugin = require("core.plugin")
 
 return plugin{
@@ -7,9 +8,37 @@ return plugin{
     style = "dark",
     highlights = {
       FoldColumn = { bg = "SignColumn" },
+      Cursor = { fg = "$bg0", bg = "$fg", fmt = "NONE" },
     },
   },
   config = function()
     require("onedark").load()
+
+    vim.opt.guicursor = "n-v-c:block-Cursor,i-ci-ve:ver25-Cursor,o:hor50-Cursor"
+
+    -- workaround for https://github.com/navarasu/onedark.nvim/issues/260
+    au.group("ThemeSync", {
+      ColorScheme = {
+        function()
+          if vim.g.colors_name ~= "onedark" or not vim.g.onedark_config then
+            return
+          end
+          local correct = vim.o.background == "light" and "light" or "dark"
+          if vim.g.onedark_config.style ~= correct then
+            local cfg = vim.g.onedark_config
+            cfg.style = correct
+            vim.g.onedark_config = cfg
+          end
+        end,
+        pattern = "onedark",
+      },
+    })
+
+    vim.api.nvim_create_autocmd("OptionSet", {
+      pattern = "background",
+      callback = function()
+        vim.cmd.colorscheme("onedark")
+      end,
+    })
   end,
 }
