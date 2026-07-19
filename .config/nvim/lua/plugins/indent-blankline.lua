182bc31633d6118614277869281f6f686a5386d0
peelz <peelz.dev@gmail.com>
2026-07-19 17:20:01 -0400

    vim: add workaround for ibl having wrong highlight group

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -6,6 +6,18 @@ return plugin{
   dependencies = {
     "nvim-treesitter",
   },
+  init = function()
+    -- workaround for https://github.com/lukas-reineke/indent-blankline.nvim/issues/986
+    local hooks = require("ibl.hooks")
+    hooks.register(hooks.type.VIRTUAL_TEXT, function(_, _, _, virt_text)
+      for _, vt in ipairs(virt_text) do
+        if vt[2] then
+          vt[2] = "IblIndent"
+        end
+      end
+      return virt_text
+    end)
+  end,
   opts = {
     indent = {
       char = "│",
